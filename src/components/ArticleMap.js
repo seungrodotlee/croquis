5a7940f05daa97143dd0259849f6235454af6ad9
이승로 <srlee0226w@gmail.com>
2020-12-23 16:31:55 +0900

    make menu response to scroll

--- src/components/ArticleMap.js
+++ src/components/ArticleMap.js
@@ -13,6 +13,7 @@ export default class ArticleMap extends TemplateElement {
         this.content = this.fromTemplate(".article-map > ul");
         this._parent = this.content;
         this._nest = [];
+        this._items = [];
       },
       dataHandler: {
         target: (newVal) => {
@@ -21,7 +22,7 @@ export default class ArticleMap extends TemplateElement {
 
           let targets = this._target.querySelectorAll("[data-depth]");
 
-          for(let i = 0; i < targets.length; i++) {
+          for (let i = 0; i < targets.length; i++) {
             let t = targets[i];
             let item = document.createElement("li");
             let link = document.createElement("a");
@@ -30,39 +31,60 @@ export default class ArticleMap extends TemplateElement {
             link.setAttribute("href", `#${targets[i].getAttribute("id")}`);
             link.textContent = targets[i].textContent;
             item.appendChild(link);
-            
-            if(this._before != undefined) {
-              if(this._before.dataset.depth < depth) {
+
+            if (this._before != undefined) {
+              if (this._before.dataset.depth < depth) {
                 let temp = this._parent;
-                
+
                 this._parent = document.createElement("ul");
                 this._lastItem.appendChild(this._parent);
 
                 this._upper = temp;
                 this._nest[this._before.dataset.depth] = temp;
-              }  
+              }
 
-              if(this._before.dataset.depth > depth) {
+              if (this._before.dataset.depth > depth) {
                 this._parent = this._nest[depth];
               }
             }
 
             link.addEventListener("click", () => {
-              if(this._current != undefined) {
-                this._current.classList.remove("current");
-              }
-
-              link.classList.add("current");
-              this._current = link;
+              this.changeCurrentTo(link);
             });
-            
+
+            this._items[t.getAttribute("id")] = link;
             this._parent.appendChild(item);
             this._before = t;
             this._lastItem = item;
           }
-        }
-      }
-    })
+
+          window.addEventListener("scroll", () => {
+            let pos = window.scrollY;
+
+            for (let i = 0; i < targets.length; i++) {
+              let item = this._items[targets[i].getAttribute("id")];
+              if (targets[i] == this._current) {
+                continue;
+              }
+
+              if (i == targets.length - 1) {
+                if (pos >= targets[i].offsetTop) {
+                  this.changeCurrentTo(item);
+                }
+              } else {
+                if (
+                  pos >= targets[i].offsetTop &&
+                  pos < targets[i + 1].offsetTop
+                ) {
+                  console.log(this._items[targets[i].getAttribute("id")]);
+                  this.changeCurrentTo(item);
+                }
+              }
+            }
+          });
+        },
+      },
+    });
   }
 
   // searchChildren(el) {
@@ -86,4 +108,13 @@ export default class ArticleMap extends TemplateElement {
   //     }
   //   }
   // }
+
+  changeCurrentTo(current) {
+    if (this._current != undefined) {
+      this._current.classList.remove("current");
+    }
+
+    current.classList.add("current");
+    this._current = current;
+  }
 }
