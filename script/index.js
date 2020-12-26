ee3c21141770c614042ce4f679724bf1e5e9e3b0
이승로 <srlee0226w@gmail.com>
2020-12-26 22:12:50 +0900

    refactor css/add navbar+sync+toast

--- script/index.js
+++ script/index.js
@@ -1,19 +1,58 @@
 window.addEventListener("load", () => {
+  let isIntroActive = JSON.parse(highway.getCookie("intro"));
   let intro = document.querySelector("#intro");
-  let promise = intro.play();
 
-  if (promise != undefined) {
-    promise
-      .then(() => {
-        console.log("play");
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }
-
-  intro.addEventListener("ended", (event) => {
+  if (isIntroActive != null && !isIntroActive) {
     let p = intro.parentElement;
     highway.removeElement(p, "hidden-by-slide-right");
+  } else {
+    intro.classList.remove("_hidden");
+    let promise = intro.play();
+
+    if (promise != undefined) {
+      promise
+        .then(() => {
+          console.log("play");
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+
+    intro.addEventListener("ended", () => {
+      let p = intro.parentElement;
+      highway.removeElement(p, "hidden-by-slide-right");
+    });
+  }
+
+  let introToggler = document.querySelector("#intro-toggler");
+
+  console.log(isIntroActive);
+  // if (isIntroActive != null) {
+  //   if (isIntroActive) {
+  //     introToggler.textContent = "인트로 안 볼래요";
+  //   } else {
+  //     introToggler.textContent = "인트로 볼래요";
+  //   }
+  // } else {
+  //   introToggler.textContent = "인트로 안 볼래요";
+  // }
+
+  if (isIntroActive != null && !isIntroActive) {
+    introToggler.textContent = "인트로 볼래요";
+  } else {
+    introToggler.textContent = "인트로 안 볼래요";
+  }
+
+  introToggler.addEventListener("click", () => {
+    if (isIntroActive != null && !isIntroActive) {
+      highway.setCookie("intro", true, 7);
+      introToggler.textContent = "인트로 안 볼래요";
+      isIntroActive = true;
+    } else {
+      highway.setCookie("intro", false, 7);
+      introToggler.textContent = "인트로 볼래요";
+      isIntroActive = false;
+    }
   });
 });
