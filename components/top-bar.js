e0999107afc6b18bdca167d5d9e17350d95bf886
이승로 <srlee0226w@gmail.com>
2021-01-05 12:53:28 +0900

    [2020.01.05.12] Sass 사이징 개선

--- components/top-bar.js
+++ components/top-bar.js
@@ -8,12 +8,25 @@ highway.newComponent("top-bar", {
       data-logo="./img/logo.svg"
     >
       <div>
-        <sync- class="no-border hover-underscore">
-          <button>알아보기</button>
-          <button>시작하기</button>
-          <button>CSS</button>
-          <button>템플릿</button>
-          <button>골라담기</button>
+        <sync- class="nav-bar-inner">
+          <div class="hidden-s">
+            <sync- class="no-border hover-underscore">
+              <button>알아보기</button>
+              <button>시작하기</button>
+              <button>CSS</button>
+              <button>템플릿</button>
+              <button>골라담기</button>
+            </sync->
+          </div>
+          <div class="only-in-s">
+            <drop-down id="mobile-menu-drop" data-title="더 알아보기">
+              <div>알아보기</div>
+              <div>시작하기</div>
+              <div>CSS</div>
+              <div>템플릿</div>
+              <div>골라담기</div>
+            </drop-down>
+          </div>
         </sync->
       </div>
       <div>
