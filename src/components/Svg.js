d1a2dc5e75beb7376955574017f9d29b6a322ca2
이승로 <srlee0226w@gmail.com>
2021-01-07 10:42:25 +0900

    [2020.01.07.10] 새로운 인트로 준비 + svg 컴포넌트 작성

--- src/components/Svg.js
+++ src/components/Svg.js
@@ -3,10 +3,35 @@ import TemplateElement from "../util/TemplateElement.js";
 class Svg extends TemplateElement {
   constructor() {
     super({
-      template: `<object type="image/svg+xml"></object>`,
+      template: `<svg></svg>`,
       dataHandler: {
         src: (newVal) => {
-          this.body.setAttribute("data", newVal);
+          this.body.removeAttribute("data-src");
+
+          let client = new XMLHttpRequest();
+          client.open("GET", "img/logo.svg");
+          client.onreadystatechange = () => {
+            if (client.readyState === 4) {
+              console.log(client);
+              let reg = new RegExp("<svg([^>]*)>((?:.|\n)*?)</svg>", "g");
+              let regResult = reg.exec(client.responseText);
+              let attrs = regResult[1];
+              let svgCode = regResult[2];
+
+              let attrsReg = new RegExp("([a-zA-Z]*)=[\"\\']([^\"']*)", "g");
+              let attrRegResult = attrsReg.exec(attrs);
+
+              while (attrRegResult != null) {
+                this.body.setAttribute(attrRegResult[1], attrRegResult[2]);
+                attrs.replace(attrRegResult[0], "");
+                attrRegResult = attrsReg.exec(attrs);
+              }
+
+              this.body.innerHTML = svgCode;
+            }
+          };
+          client.send();
+          //this.body.setAttribute("data", newVal);
         },
       },
     });
