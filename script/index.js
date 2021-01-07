e8968bf1abd039780099f55babd51b8074bfcf42
이승로 <srlee0226w@gmail.com>
2021-01-07 16:39:37 +0900

    [2020.01.07.16] 로고/인트로 업데이트

--- script/index.js
+++ script/index.js
@@ -6,54 +6,31 @@ croquis.abc = {
   test3: "c",
 };
 
-window.addEventListener("load", () => {
-  let introSloganTop = document.querySelector("#intro-slogan-top");
-  let introSloganBottom = document.querySelector("#intro-slogan-bottom");
-  let p = introSloganTop.geul("당신의 도화지를");
-
-  p.then((r) => {
-    introSloganBottom.geul("더 쉽게 채우기 위한 도구");
-  });
-
-  return;
+window.addEventListener("load", async () => {
   let isIntroActive = JSON.parse(croquis.getCookie("intro"));
-  let intro = document.querySelector("#intro");
+  let introWrap = document.querySelector(".intro-wrap");
 
   if (isIntroActive != null && !isIntroActive) {
-    let p = intro.parentElement;
-    croquis.removeElement(p, "hidden-by-slide-right");
+    croquis.removeElement(introWrap, "hidden-by-slide-right");
   } else {
-    intro.classList.remove("_hidden");
-    let promise = intro.play();
+    let introSloganTop = document.querySelector("#intro-slogan-top");
+    let introSloganBottom = document.querySelector("#intro-slogan-bottom");
+    let introLogo = document.querySelector("#intro-logo");
 
-    if (promise != undefined) {
-      promise
-        .then(() => {
-          console.log("play");
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-    }
+    await introSloganTop.geul("당신의 도화지를", 80);
+    introLogo.setAttribute("style", "stroke-dashoffset: 0;");
+    await introSloganBottom.geul("채우는 가장 쉬운 방법", 80, 500);
 
-    intro.addEventListener("ended", () => {
-      let p = intro.parentElement;
-      croquis.removeElement(p, "hidden-by-slide-right");
-    });
+    await croquis.delay(() => {
+      introLogo.setAttribute("style", "stroke-dashoffset: -1700;");
+    }, 1000);
+
+    croquis.delay(() => {
+      croquis.removeElement(introWrap, "hidden-by-offset-slide-right");
+    }, 1000);
   }
 
   let introToggler = document.querySelector("#intro-toggler");
-
-  console.log(isIntroActive);
-  // if (isIntroActive != null) {
-  //   if (isIntroActive) {
-  //     introToggler.textContent = "인트로 안 볼래요";
-  //   } else {
-  //     introToggler.textContent = "인트로 볼래요";
-  //   }
-  // } else {
-  //   introToggler.textContent = "인트로 안 볼래요";
-  // }
 
   if (isIntroActive != null && !isIntroActive) {
     introToggler.textContent = "인트로 볼래요";
