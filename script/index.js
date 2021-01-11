3ae13ebe6b3b7352393d831cf8f8fcea39a48c2c
이승로 <srlee0226w@gmail.com>
2021-01-11 15:51:21 +0900

    [202.01.11.15] 로고/비주얼 업데이트

--- script/index.js
+++ script/index.js
@@ -6,31 +6,40 @@ croquis.abc = {
   test3: "c",
 };
 
-window.addEventListener("load", async () => {
+window.addEventListener("load", () => {
   let isIntroActive = JSON.parse(croquis.getCookie("intro"));
   let introWrap = document.querySelector(".intro-wrap");
-
-  if (isIntroActive != null && !isIntroActive) {
-    croquis.removeElement(introWrap, "hidden-by-slide-right");
-  } else {
-    let introSloganTop = document.querySelector("#intro-slogan-top");
-    let introSloganBottom = document.querySelector("#intro-slogan-bottom");
-    let introLogo = document.querySelector("#intro-logo");
-
-    await introSloganTop.geul("당신의 도화지를", 80);
-    introLogo.setAttribute("style", "stroke-dashoffset: 0;");
-    await introSloganBottom.geul("채우는 가장 쉬운 방법", 80, 500);
-
-    await croquis.delay(() => {
-      introLogo.setAttribute("style", "stroke-dashoffset: -1700;");
-    }, 1000);
-
-    croquis.delay(() => {
-      croquis.removeElement(introWrap, "hidden-by-offset-slide-right");
-    }, 1000);
-  }
-
+  let introSloganTop = document.querySelector("#intro-slogan-top");
+  let introSloganBottom = document.querySelector("#intro-slogan-bottom");
+  let introLogo = document.querySelector("#intro-logo");
   let introToggler = document.querySelector("#intro-toggler");
+
+  let bigLogo = document.querySelector("#bg-logo");
+  let sloganWrap = document.querySelector(".main-slogan");
+  let mainSloganTop = document.querySelector("#slogan-top");
+  let mainSloganBottom = document.querySelector("#slogan-bottom");
+
+  setTimeout(() => {
+    window.scrollTo(0, 0);
+  }, 100);
+
+  (async () => {
+    if (isIntroActive != null && !isIntroActive) {
+      croquis.removeElement(introWrap, "hidden-by-slide-right");
+    } else {
+      await introSloganTop.geul("당신의 도화지를", 80);
+      introLogo.setAttribute("style", "stroke-dashoffset: 0;");
+      await introSloganBottom.geul("채우는 가장 쉬운 방법", 80, 500);
+
+      // await croquis.delay(() => {
+      //   introLogo.setAttribute("style", "stroke-dashoffset: -1700;");
+      // }, 1000);
+
+      croquis.delay(() => {
+        croquis.removeElement(introWrap, "hidden-by-offset-slide-right");
+      }, 1000);
+    }
+  })();
 
   if (isIntroActive != null && !isIntroActive) {
     introToggler.textContent = "인트로 볼래요";
@@ -47,6 +56,28 @@ window.addEventListener("load", async () => {
       croquis.setCookie("intro", false, 7);
       introToggler.textContent = "인트로 볼래요";
       isIntroActive = false;
+    }
+  });
+
+  window.addEventListener("scroll", (e) => {
+    let h = window.innerHeight;
+    let pos = window.scrollY;
+
+    if (pos >= 0 && pos <= h) {
+      let xTarget =
+        parseInt(
+          window.getComputedStyle(mainSloganTop).width.replace("px", "")
+        ) * 1.1;
+      let ratio = pos / h;
+
+      sloganWrap.setAttribute(
+        "style",
+        `top: calc(${50 - 50 * ratio}% + ${
+          9 * ratio
+        }rem); transform: translateY(-50%) scale(${1 - 0.25 * ratio})`
+      );
+
+      bigLogo.setAttribute("style", `stroke-dashoffset: -${1700 * ratio};`);
     }
   });
 });
