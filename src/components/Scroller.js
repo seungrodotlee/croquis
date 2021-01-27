94993ed8641575541aab81a425899d2860790761
이승로 <srlee0226w@gmail.com>
2021-01-27 15:40:50 +0900

    [2021.01.27:15] slider 수정

--- src/components/Scroller.js
+++ src/components/Scroller.js
@@ -5,13 +5,13 @@ class Scroller extends TemplateElement {
     super({
       template: `
         <div class="scroller">
-          <button class="scroller-prev-btn">
+          <button class="scroller-prev-btn centered-y">
             <span class="material-icons">keyboard_arrow_left</span>
           </button>
-          <button class="scroller-next-btn">
+          <button class="scroller-next-btn centered-y">
             <span class="material-icons">keyboard_arrow_right</span>
           </button>
-          <div class="scroller-content"></div>
+          <div class="scroller-content item-contain"></div>
         </div>
       `,
       templateHandler: () => {
@@ -23,6 +23,11 @@ class Scroller extends TemplateElement {
         this.length = 0;
         this.pages = [];
         this.delay = 3000;
+
+        this.smallestWidth = 0;
+        this.biggestWidth = 0;
+        this.smallestHeight = 0;
+        this.biggestHeight = 0;
 
         this.prevBtn.addEventListener("click", () => {
           if (this.current != 0) {
@@ -47,8 +52,38 @@ class Scroller extends TemplateElement {
         });
       },
       childHandler: (addedNode) => {
+        let originDisplay = addedNode.style.display;
+        addedNode.style.display = "inline-block";
+        console.dir(addedNode);
+        console.log(addedNode.clientWidth);
+
+        let h = addedNode.clientHeight;
+        let w = addedNode.clientWidth;
+
+        addedNode.style.display = originDisplay;
+
         if (this.length == 0) {
           addedNode.classList.add("current");
+          this.smallestHeight = h;
+          this.biggestHeight = h;
+          this.smallestWidth = w;
+          this.biggestWidth = w;
+        } else {
+          if (h < this.smallestHeight) {
+            this.smallestHeight = h;
+          }
+
+          if (h > this.biggestHeight) {
+            this.biggestHeight = h;
+          }
+
+          if (w < this.smallestWidth) {
+            this.smallestWidth = w;
+          }
+
+          if (w > this.biggestWidth) {
+            this.biggestWidth = w;
+          }
         }
 
         this.pages[this.length] = addedNode;
@@ -77,6 +112,39 @@ class Scroller extends TemplateElement {
             }, this.delay);
           } else {
             clearInterval(this.interval);
+          }
+        },
+        align: (newVal) => {
+          if (newVal === "min") {
+            this.body.classList.remove("item-fit-contain");
+            this.body.classList.add("item-fit-cover");
+            this.body.style.height = this.smallestHeight + "px";
+            this.body.style.width = this.smallestWidth + "px";
+          }
+
+          if (newVal === "centering") {
+            this.body.classList.add("item-fit-contain");
+            this.body.classList.remove("item-fit-cover");
+            this.body.style.height = this.biggestHeight + "px";
+            this.body.style.width = this.biggestWidth + "px";
+          }
+
+          if (newVal === "max") {
+            this.body.classList.remove("item-fit-contain");
+            this.body.classList.add("item-fit-cover");
+            this.body.style.height = this.biggestHeight + "px";
+            this.body.style.width = this.biggestWidth + "px";
+          }
+        },
+        fit: (newVal) => {
+          if (newVal === "centering") {
+            this.body.classList.add("item-fit-contain");
+            this.body.classList.remove("item-fit-cover");
+          }
+
+          if (newVal == "cover") {
+            this.body.classList.remove("item-fit-contain");
+            this.body.classList.add("item-fit-cover");
           }
         },
         ratio: (newVal) => {
